bf0c45b032d3f2d06192394cc79320078a6d24ad
Willian Krause <williankrause@MacBook-Pro-de-Willian.local>
2019-09-02 13:51:01 -0300

    Updates

---
+++ precache-manifest.eb53edce2af861d956db45bc4dc7a0e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac145f3d5e00d410507f64a7d3da36c",
+    "url": "/recibo-generator/index.html"
+  },
+  {
+    "revision": "4c6a741ea7b1e63f71de",
+    "url": "/recibo-generator/static/css/main.c0c5d6f4.chunk.css"
+  },
+  {
+    "revision": "31d3329214922e54c075",
+    "url": "/recibo-generator/static/js/2.3a40ad0f.chunk.js"
+  },
+  {
+    "revision": "4c6a741ea7b1e63f71de",
+    "url": "/recibo-generator/static/js/main.efbe2b4d.chunk.js"
+  },
+  {
+    "revision": "333b9686462d74ded9bf",
+    "url": "/recibo-generator/static/js/runtime~main.f2ae6295.js"
+  }
+]);
